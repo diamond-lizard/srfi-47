a23d307cbdd9c3cefa33c67891020bee1ccba90b
Cthulhu Ftaghn <cthulhu@ryleh.example.com>
2020-11-16 14:12:27 -0800

    Removed (uses srfi-4) as srfi-4 is built in to Chicken 5

--- srfi-47.scm
+++ srfi-47.scm
@@ -22,7 +22,6 @@
 
 (declare
  (fixnum)
- (uses srfi-4)
  (export make-array array-ref array-set! make-shared-array array?
 	 array-rank array-dimensions array-in-bounds? array-store
 	 at1 au8 au16 au32 au64 as8 as16 as32 as64 ar32 ar64) )
