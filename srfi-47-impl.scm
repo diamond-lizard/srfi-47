5e53d05529262f83554797838f6bf602a7344767
Cthulhu Ftaghn <cthulhu@ryleh.example.com>
2020-11-16 14:19:47 -0800

    Created module in srfi-47.scm and moved exports there

--- srfi-47-impl.scm
+++ srfi-47-impl.scm
@@ -20,11 +20,6 @@
 ;;@code{(require 'array)}
 ;;@ftindex array
 
-(declare
- (fixnum)
- (export make-array array-ref array-set! make-shared-array array?
-	 array-rank array-dimensions array-in-bounds? array-store
-	 at1 au8 au16 au32 au64 as8 as16 as32 as64 ar32 ar64) )
 
 (define-record array shape scales offset store tmap)
 
